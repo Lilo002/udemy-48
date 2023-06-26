edc2dcac85495e9de9257ecd34addcdeb97e0ec1
Lilo002 <lisabasarab@gmail.com>
2023-06-26 16:44:44 +0300

    feat: animation(Mon, Jun 26, 2023  4:44:43 PM)

--- src/js/script.js
+++ src/js/script.js
@@ -164,49 +164,166 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Forms
 
-    const forms = document.querySelectorAll('.feed-form'),
-          openGratitudeBtn = document.querySelectorAll('[data-submit]');
+    const forms = document.querySelectorAll('.feed-form');
           
     forms.forEach(item => {
         bindPostData(item);
     });
-    
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            /* mode: 'no-cors', */
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: data
-        });
-
-        return await res.json();
-    };
 
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
+            if(overlay.dataset.close !== '' && overlay.dataset.close) {
+                closeModal(overlay.dataset.close);
+            }
+
+            openModal('loading');
+
             const formData = new FormData(form);
 
-            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            /* const json = JSON.stringify(Object.fromEntries(formData.entries())); */
+            //если json то добавить JSON.stringify() и довавить заголовок и в выводе результата добавить res.json()
+            /* http://localhost:3000/request */
 
-            postData('http://localhost:3000/request', json)
+            fetch('mailer/smart.php', {
+                method: "POST",
+                body: formData
+            })
             .then(data => {
-                if(overlay.dataset.close !== '' && overlay.dataset.close) {
-                    closeModal(overlay.dataset.close);
-                }
+                closeModal('loading');
                 openModal('gratitude');
             }).catch(() => {
-                if(overlay.dataset.close !== '' && overlay.dataset.close) {
-                    closeModal(overlay.dataset.close);
-                }
+                closeModal('loading');
                 openModal('error');
             }).finally(() => {
                 form.reset();
             })
         });
-    }   
+    }
+    
+    //validation
+
+    const telInput = document.querySelectorAll('[data-input-tel]');
+
+    function getInputNumbersValue(input) {
+        return input.value.replace(/\D/g, '');
+    }
+
+    function onPhoneInput(e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            formatedInputValue = '',
+            selectionStart = input.selectionStart;
+
+        if(input.value.length != selectionStart) {
+            if(e.data && /\D/g.test(e.data)) {
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if(['3', '+'].includes(inputNumbersValue[0], 0)) {
+            //Belarus number
+
+            let firstSymbol = `+375 `;
+            formatedInputValue = firstSymbol;
+
+            if(inputNumbersValue.length > 3) {
+                formatedInputValue += `(${inputNumbersValue.substring(3,5)}`;
+            }
+            if(inputNumbersValue.length > 5) {
+                formatedInputValue += `) ${inputNumbersValue.substring(5,8)}`;
+            }
+            if(inputNumbersValue.length > 8) {
+                formatedInputValue += `-${inputNumbersValue.substring(8,10)}`;
+            }
+            if(inputNumbersValue.length > 10) {
+                formatedInputValue += `-${inputNumbersValue.substring(10,12)}`;
+            }
+
+        } else if(inputNumbersValue[0] == '8') {
+
+            let firstSymbol = `8 `;
+            formatedInputValue = firstSymbol;
+
+            if(inputNumbersValue.length > 1) {
+                formatedInputValue += `${inputNumbersValue.substring(1,4)}`;
+            }
+            if(inputNumbersValue.length > 4) {
+                formatedInputValue += ` ${inputNumbersValue.substring(4,11)}`;
+            }           
+        } 
+        else if(input.value == '' || input.value[0] == '+' && input.value.length == 2) {
+
+            return input.value = '';
+
+        } else {
+            //else number
+            formatedInputValue = `+ ${inputNumbersValue.substring(0, 16)}`;
+
+        }
+
+        input.value = formatedInputValue;    
+    }
+
+    function onPhoneKeyDown(e) {
+        // удаление если осталась шапка:
+        if(e.keyCode == 8 && (getInputNumbersValue(e.target).length == 1 || e.target.value == `+375 `)) {
+            e.target.value = '';
+        }
+    };
+
+    function onPhonePaste(e) {
+        let pasted = e.clipboardData || window.clipboardData,
+            input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+
+        if(pasted) {
+            let pastedText = pasted.getData("Text");
+            if(/\D/g.test(pastedText)) {
+                input.value = inputNumbersValue;
+            }
+        }
+    }
+
+    telInput.forEach((item, index) => {
+        item.addEventListener('keydown', onPhoneKeyDown);
+        item.addEventListener('input', onPhoneInput, false);
+        item.addEventListener('paste', onPhonePaste, false);
+    });
+
+    //reviews animation fadeInUp (animate__fadeInUp animate__animated)
+
+    const reviews = document.querySelectorAll('.review'),
+          reviewSection = document.querySelector('.reviews');
+
+    document.addEventListener('scroll', onScroll);
+
+    function addAnimation(item) {
+        item.classList.add('animate__fadeInUp', 'review_show');
+    }
+          
+    function onScroll(e) {
+        let currentPosition = window.scrollY;
+
+        if((reviewSection.offsetTop - reviewSection.offsetHeight) <= currentPosition && (reviewSection.offsetTop + reviewSection.offsetHeight) > currentPosition) {
+            reviews.forEach((item, i) => {
+                if((item.offsetTop - reviewSection.offsetHeight / 1.2) <= currentPosition) {
+                    let timmer = i * 300 + 300;
+
+                    console.log(item.offsetTop);
+                    console.log(item.offsetHeight);
+                    console.log(currentPosition);
+
+                    setTimeout(addAnimation, timmer, item);
+
+                    
+                }
+            })
+        /* document.removeEventListener('scroll', onScroll); */
+        }
+            
+    }
 
 });
